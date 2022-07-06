9216924d5514b3724f6c571ff34c20603dea2d79
raoelsonnyaro1 <raoelsonnyaro@gmail.com>
2022-07-06 22:41:45 +0300

    first component

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # typotab
-exo 04/07/2022
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <base-typo msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import BaseTypo from "./components/BaseTypo.vue";
+
+export default {
+  name: "App",
+  components: {
+    BaseTypo,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
